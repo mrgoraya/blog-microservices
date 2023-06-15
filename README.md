0727c9a0814c5a0e61057719bc7d9b20e265ccdc
Mehran Rasheed <engr.mehranrasheed@gmail.com>
2023-06-15 13:31:30 +0500

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Blog
+
+The repository is created to implement the microservice architechture. The event bus would be created manually using Javascript and express.
+
+## Client
+
+Client folder is the React App that interact with Posts service and Comments service to render the posts and comments.
+
+## Posts service
+
+Separate service created that has get and post endpoints to create and post the posts.
+
+## Comments service
+
+Separate service created that has get and post endpoints to create and post the comments for each posts.
---
+++ comments/index.js
@@ -0,0 +1,33 @@
+import express from "express";
+import bodyParser from "body-parser";
+import { randomBytes } from "crypto";
+import cors from "cors";
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+const commentsByPostId = {};
+
+app.get("/posts/:id/comments", (req, res) => {
+  res.send(commentsByPostId[req.params.id] || []);
+});
+
+app.post("/posts/:id/comments", (req, res) => {
+  const commentId = randomBytes(4).toString("hex");
+
+  const { content } = req.body;
+  const { id } = req.params;
+
+  const comments = commentsByPostId[id] || [];
+
+  comments.push({ id: commentId, content });
+
+  commentsByPostId[id] = comments;
+
+  res.status(201).send(comments);
+});
+
+app.listen(4001, () => {
+  console.log("Listening on 4001");
+});
