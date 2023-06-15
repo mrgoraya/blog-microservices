fc214003a5a26b4e31ed7b16583c258362cdcb31
Mehran Rasheed <engr.mehranrasheed@gmail.com>
2023-06-15 16:51:29 +0500

    Implemented query service.

---
+++ query-service/index.js
@@ -0,0 +1,40 @@
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+
+const PORT = 4002;
+
+const posts = {};
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get("/posts", (req, res) => {
+  res.send(posts);
+});
+
+app.post("/events", (req, res) => {
+  const { type, data } = req.body;
+
+  if (type === "PostCreated") {
+    const { id, title } = data;
+
+    posts[id] = { id, title, comments: [] };
+  }
+
+  if (type === "CommentCreated") {
+    const { id, content, postId } = data;
+
+    const post = posts[postId];
+    post.comments.push({ id, content });
+  }
+
+  console.log(posts);
+
+  res.send({});
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on port: ${PORT}`);
+});
